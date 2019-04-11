ba61b0a74bcb08efe9fbdf991ab2be0b0923a58e
17kikak <kikakovaleski@gmail.com>
2019-04-11 19:37:00 -0400

    got recipes index to show name and url

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "/recipes", to: "recipes#index"
+  get "/tv-shows", to: "tvshows#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
