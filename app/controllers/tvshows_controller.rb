04d181868226bac7466fb1f42a93a190b3dc0b7f
17kikak <kikakovaleski@gmail.com>
2019-04-12 19:11:48 -0400

    set up show pages

--- app/controllers/tvshows_controller.rb
+++ app/controllers/tvshows_controller.rb
@@ -2,4 +2,8 @@ class TvshowsController < ApplicationController
   def index
     @tvshows = Tvshow.all
   end
+  def show
+    @tvshows = Tvshow.all
+    @showid = Tvshow.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/recipes", to: "recipes#index"
   get "/tv-shows", to: "tvshows#index"
+  get "/recipes/:id", to: "recipes#show"
+  get "/tv-shows/:id", to: "tvshows#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
