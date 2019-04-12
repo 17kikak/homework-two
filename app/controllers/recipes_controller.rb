4130e6f474a37c7ba577c6d075b6202553b9ac12
17kikak <kikakovaleski@gmail.com>
2019-04-12 19:24:14 -0400

    got show to work for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,8 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+  def show
+    @tvshows = Recipe.all
+    @recipeid = Recipe.find(params[:id])
+  end
 end
